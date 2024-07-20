2435567e51439efafa49cc7d72dbea5653dab7c6
Kateryna-Zazuliak <kateryna_didenko@ukr.net>
2024-07-21 02:35:32 +0300

    add tasks

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,25 @@
+import css from "./TransactionHistory.module.css";
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.table}>
+      <thead className={css.head}>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      {items.map(({ id, type, amount, currency }) => (
+        <tbody key={id}>
+          <tr>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        </tbody>
+      ))}
+    </table>
+  );
+};
+export default TransactionHistory;
