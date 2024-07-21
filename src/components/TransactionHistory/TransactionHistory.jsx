2a9ad24636b74fbfd983f0ada4d4b4c5a603b8a8
Kateryna-Zazuliak <kateryna_didenko@ukr.net>
2024-07-21 03:01:58 +0300

    add changes

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -3,22 +3,22 @@ import css from "./TransactionHistory.module.css";
 const TransactionHistory = ({ items }) => {
   return (
     <table className={css.table}>
-      <thead className={css.head}>
-        <tr>
+      <thead>
+        <tr className={css.head}>
           <th>Type</th>
           <th>Amount</th>
           <th>Currency</th>
         </tr>
       </thead>
-      {items.map(({ id, type, amount, currency }) => (
-        <tbody key={id}>
-          <tr>
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id}>
             <td>{type}</td>
             <td>{amount}</td>
             <td>{currency}</td>
           </tr>
-        </tbody>
-      ))}
+        ))}
+      </tbody>
     </table>
   );
 };
